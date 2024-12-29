2473fdd54ab07a64a7250ea49b974a1b9ea7fb73
Tuong Huynh Manh <huynhmanhtuong1180@gmail.com>
2024-12-29 09:40:30 +0700

    public ratings api

--- src/controllers/rating.controller.ts
+++ src/controllers/rating.controller.ts
@@ -1,12 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import Authenticate from "../decorators/authenticate";
 import Authorize from "../decorators/authorize";
 import Controller from "../decorators/controller";
 import { Get, Post } from "../decorators/handlers";
 import { SystemRole } from "../utils/enums";
 import RatingRepository from "../database/repositories/rating.repository";
 
-Authenticate();
 @Controller("/ratings")
 export default class RatingController {
   @Post("/:idProduct")
